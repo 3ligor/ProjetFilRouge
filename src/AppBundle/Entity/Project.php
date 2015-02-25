27dbc2e27dccae9cef304da844998e000789afca
julien <julien@Imideb>
2015-02-25 10:08:07 +0100

    Project Views, entity, fixtures Mapping project-skill

--- src/AppBundle/Entity/Project.php
+++ src/AppBundle/Entity/Project.php
@@ -13,79 +13,79 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="ProjectRepository")
  */
-class Project
-{
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+class Project {
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=255)
-     */
-    private $title;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="smallDescription", type="string", length=255)
-     */
-    private $smallDescription;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="title", type="string", length=255)
+	 */
+	private $title;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="longDescription", type="text")
-     */
-    private $longDescription;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="smallDescription", type="string", length=255)
+	 */
+	private $smallDescription;
 
-    /**
-     * @var DateTime
-     *
-     * @ORM\Column(name="startDate", type="datetime")
-     */
-    private $startDate;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="longDescription", type="text")
+	 */
+	private $longDescription;
 
-    /**
-     * @var DateTime
-     *
-     * @ORM\Column(name="endDate", type="datetime")
-     */
-    private $endDate;
+	/**
+	 * @var DateTime
+	 *
+	 * @ORM\Column(name="startDate", type="datetime")
+	 */
+	private $startDate;
 
-    /**
-     * @var DateTime
-     *
-     * @ORM\Column(name="creationDate", type="datetime")
-     */
-    private $creationDate;
+	/**
+	 * @var DateTime
+	 *
+	 * @ORM\Column(name="endDate", type="datetime")
+	 */
+	private $endDate;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="maxMembers", type="integer")
-     */
-    private $maxMembers;
+	/**
+	 * @var DateTime
+	 *
+	 * @ORM\Column(name="creationDate", type="datetime")
+	 */
+	private $creationDate;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="status", type="integer")
-     */
-    private $status;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="maxMembers", type="integer")
+	 */
+	private $maxMembers;
 
-    /**
-     * @var array
-     *
-     * @ORM\ManyToMany(targetEntity="Category", inversedBy="projects")
-     */
-    private $categories;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="status", type="integer")
+	 */
+	private $status;
+
+	/**
+	 * @var array
+	 *
+	 * @ORM\ManyToMany(targetEntity="Category", inversedBy="projects")
+	 */
+	private $categories;
 
 	/**
 	 * @var User
@@ -100,7 +100,7 @@ class Project
 	 * @ORM\OneToMany(targetEntity="Stage", mappedBy="project", orphanRemoval=true)
 	 */
 	private $stages;
-	
+
 	/**
 	 *
 	 * @var array
@@ -109,323 +109,383 @@ class Project
 	 */
 	private $userProjects;
 	
-    /**
-     * Constructor
-     */
-    public function __construct() {
-        $this->categories = new ArrayCollection();
-        $this->members = new ArrayCollection();
-		$this->stages = new ArrayCollection();		
+	/**
+	 * @var array
+	 * 
+	 * @ORM\ManyToMany(targetEntity="Skill", inversedBy="projects")
+	 */
+	private $skills;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->categories = new ArrayCollection();
+		$this->members = new ArrayCollection();
+		$this->stages = new ArrayCollection();
 		$this->userProjects = new ArrayCollection();
-    }
+		$this->skills = new ArrayCollection();
+	}
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId() {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return integer 
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-    /**
-     * Set title
-     *
-     * @param string $title
-     * @return Project
-     */
-    public function setTitle($title) {
-        $this->title = $title;
-        return $this;
-    }
+	/**
+	 * Set title
+	 *
+	 * @param string $title
+	 * @return Project
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+		return $this;
+	}
 
-    /**
-     * Get title
-     *
-     * @return string 
-     */
-    public function getTitle() {
-        return $this->title;
-    }
+	/**
+	 * Get title
+	 *
+	 * @return string 
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
 
-    /**
-     * Set smallDescription
-     *
-     * @param string $smallDescription
-     * @return Project
-     */
-    public function setSmallDescription($smallDescription) {
-        $this->smallDescription = $smallDescription;
-        return $this;
-    }
+	/**
+	 * Set smallDescription
+	 *
+	 * @param string $smallDescription
+	 * @return Project
+	 */
+	public function setSmallDescription($smallDescription) {
+		$this->smallDescription = $smallDescription;
+		return $this;
+	}
 
-    /**
-     * Get smallDescription
-     *
-     * @return string 
-     */
-    public function getSmallDescription() {
-        return $this->smallDescription;
-    }
+	/**
+	 * Get smallDescription
+	 *
+	 * @return string 
+	 */
+	public function getSmallDescription() {
+		return $this->smallDescription;
+	}
 
-    /**
-     * Set longDescription
-     *
-     * @param string $longDescription
-     * @return Project
-     */
-    public function setLongDescription($longDescription) {
-        $this->longDescription = $longDescription;
-        return $this;
-    }
+	/**
+	 * Set longDescription
+	 *
+	 * @param string $longDescription
+	 * @return Project
+	 */
+	public function setLongDescription($longDescription) {
+		$this->longDescription = $longDescription;
+		return $this;
+	}
 
-    /**
-     * Get longDescription
-     *
-     * @return string 
-     */
-    public function getLongDescription() {
-        return $this->longDescription;
-    }
+	/**
+	 * Get longDescription
+	 *
+	 * @return string 
+	 */
+	public function getLongDescription() {
+		return $this->longDescription;
+	}
 
-    /**
-     * Set startDate
-     *
-     * @param DateTime $startDate
-     * @return Project
-     */
-    public function setStartDate($startDate) {
-        $this->startDate = $startDate;
-        return $this;
-    }
+	/**
+	 * Set startDate
+	 *
+	 * @param DateTime $startDate
+	 * @return Project
+	 */
+	public function setStartDate($startDate) {
+		$this->startDate = $startDate;
+		return $this;
+	}
 
-    /**
-     * Get startDate
-     *
-     * @return DateTime 
-     */
-    public function getStartDate() {
-        return $this->startDate;
-    }
+	/**
+	 * Get startDate
+	 *
+	 * @return DateTime 
+	 */
+	public function getStartDate() {
+		return $this->startDate;
+	}
 
-    /**
-     * Set endDate
-     *
-     * @param DateTime $endDate
-     * @return Project
-     */
-    public function setEndDate($endDate) {
-        $this->endDate = $endDate;
-        return $this;
-    }
+	/**
+	 * Set endDate
+	 *
+	 * @param DateTime $endDate
+	 * @return Project
+	 */
+	public function setEndDate($endDate) {
+		$this->endDate = $endDate;
+		return $this;
+	}
 
-    /**
-     * Get endDate
-     *
-     * @return DateTime 
-     */
-    public function getEndDate() {
-        return $this->endDate;
-    }
+	/**
+	 * Get endDate
+	 *
+	 * @return DateTime 
+	 */
+	public function getEndDate() {
+		return $this->endDate;
+	}
 
-    /**
-     * Set creationDate
-     *
-     * @param DateTime $creationDate
-     * @return Project
-     */
-    public function setCreationDate($creationDate) {
-        $this->creationDate = $creationDate;
-        return $this;
-    }
+	/**
+	 * Set creationDate
+	 *
+	 * @param DateTime $creationDate
+	 * @return Project
+	 */
+	public function setCreationDate($creationDate) {
+		$this->creationDate = $creationDate;
+		return $this;
+	}
 
-    /**
-     * Get creationDate
-     *
-     * @return DateTime 
-     */
-    public function getCreationDate() {
-        return $this->creationDate;
-    }
+	/**
+	 * Get creationDate
+	 *
+	 * @return DateTime 
+	 */
+	public function getCreationDate() {
+		return $this->creationDate;
+	}
 
-    /**
-     * Set maxMembers
-     *
-     * @param integer $maxMembers
-     * @return Project
-     */
-    public function setMaxMembers($maxMembers) {
-        $this->maxMembers = $maxMembers;
-        return $this;
-    }
+	/**
+	 * Set maxMembers
+	 *
+	 * @param integer $maxMembers
+	 * @return Project
+	 */
+	public function setMaxMembers($maxMembers) {
+		$this->maxMembers = $maxMembers;
+		return $this;
+	}
 
-    /**
-     * Get maxMembers
-     *
-     * @return integer 
-     */
-    public function getMaxMembers() {
-        return $this->maxMembers;
-    }
+	/**
+	 * Get maxMembers
+	 *
+	 * @return integer 
+	 */
+	public function getMaxMembers() {
+		return $this->maxMembers;
+	}
 
-    /**
-     * Set status
-     *
-     * @param integer $status
-     * @return Project
-     */
-    public function setStatus($status) {
-        $this->status = $status;
-        return $this;
-    }
+	/**
+	 * Set status
+	 *
+	 * @param integer $status
+	 * @return Project
+	 */
+	public function setStatus($status) {
+		$this->status = $status;
+		return $this;
+	}
 
-    /**
-     * Get status
-     *
-     * @return integer 
-     */
-    public function getStatus() {
-        return $this->status;
-    }
+	/**
+	 * Get status
+	 *
+	 * @return integer 
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
 
-    /**
-     * Add categories
-     *
-     * @param Category $categories
-     * @return Project
-     */
-    public function addCategory(Category $categories) {
-        $this->categories[] = $categories;
+	/**
+	 * Add categories
+	 *
+	 * @param Category $categories
+	 * @return Project
+	 */
+	public function addCategory(Category $categories) {
+		$this->categories[] = $categories;
 		$categories->addProject($this);
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Remove categories
-     *
-     * @param Category $categories
-     */
-    public function removeCategory(Category $categories) {
-        $this->categories->removeElement($categories);
-    }
+	/**
+	 * Remove categories
+	 *
+	 * @param Category $categories
+	 */
+	public function removeCategory(Category $categories) {
+		$this->categories->removeElement($categories);
+	}
 
-    /**
-     * Get categories
-     *
-     * @return Collection 
-     */
-    public function getCategories() {
-        return $this->categories;
-    }
+	/**
+	 * Get categories
+	 *
+	 * @return Collection 
+	 */
+	public function getCategories() {
+		return $this->categories;
+	}
 
-    /**
-     * Add members
-     *
-     * @param User $members
-     * @return Project
-     */
-    public function addMember(User $members) {
-        $this->members[] = $members;
+	/**
+	 * Add members
+	 *
+	 * @param User $members
+	 * @return Project
+	 */
+	public function addMember(User $members) {
+		$this->members[] = $members;
 		$members->addProject($this);
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Remove members
-     *
-     * @param User $members
-     */
-    public function removeMember(User $members) {
-        $this->members->removeElement($members);
-    }
+	/**
+	 * Remove members
+	 *
+	 * @param User $members
+	 */
+	public function removeMember(User $members) {
+		$this->members->removeElement($members);
+	}
 
-    /**
-     * Get members
-     *
-     * @return Collection 
-     */
-    public function getMembers() {
-        return $this->members;
-    }
+	/**
+	 * Get members
+	 *
+	 * @return Collection 
+	 */
+	public function getMembers() {
+		return $this->members;
+	}
 
-    /**
-     * Set leader
-     *
-     * @param User $leader
-     * @return Project
-     */
-    public function setLeader(User $leader = null) {
-        $this->leader = $leader;
+	/**
+	 * Set leader
+	 *
+	 * @param User $leader
+	 * @return Project
+	 */
+	public function setLeader(User $leader = null) {
+		$this->leader = $leader;
 		$leader->addLeadProject($this);
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get leader
-     *
-     * @return User 
-     */
-    public function getLeader() {
-        return $this->leader;
-    }
+	/**
+	 * Get leader
+	 *
+	 * @return User 
+	 */
+	public function getLeader() {
+		return $this->leader;
+	}
 
-    /**
-     * Add stage
-     *
-     * @param Stage $stage
-     * @return Project
-     */
-    public function addStage(Stage $stage) {
-        $this->stages[] = $stage;
+	/**
+	 * Add stage
+	 *
+	 * @param Stage $stage
+	 * @return Project
+	 */
+	public function addStage(Stage $stage) {
+		$this->stages[] = $stage;
 		$stage->setProject($this);
-        return $this;
-    }
+		return $this;
+	}
+
+	/**
+	 * Removes stage
+	 *
+	 * @param Stage $stage
+	 */
+	public function removeStage(Stage $stage) {
+		$this->stages->removeElement($stage);
+	}
+
+	/**
+	 * Get stage
+	 *
+	 * @return Collection 
+	 */
+	public function getStages() {
+		return $this->stages;
+	}
+
+	/**
+	 * Add userProjects
+	 *
+	 * @param \AppBundle\Entity\UserProject $userProjects
+	 * @return Project
+	 */
+	public function addUserProject(\AppBundle\Entity\UserProject $userProjects) {
+		$this->userProjects[] = $userProjects;
+		return $this;
+	}
+
+	/**
+	 * Remove userProjects
+	 *
+	 * @param \AppBundle\Entity\UserProject $userProjects
+	 */
+	public function removeUserProject(\AppBundle\Entity\UserProject $userProjects) {
+		$this->userProjects->removeElement($userProjects);
+	}
+
+	/**
+	 * Get userProjects
+	 *
+	 * @return \Doctrine\Common\Collections\Collection 
+	 */
+	public function getUserProjects() {
+		return $this->userProjects;
+	}
+
+	public function getProgress() {
+		$progress = array(
+			'term' => 0,
+			'current' => 0,
+			'left' => 0);
+		
+		$check = true;
+		
+		foreach ($this->getStages() as $stage) {
+			if ($stage->getStatus()) {
+				$progress['term'] += $stage->getVolume();
+			} elseif ($check) {
+				$check = false;
+				$progress['current'] += $stage->getVolume();
+			} else {
+				$progress['left'] += $stage->getVolume();
+			}
+		}
+		
+		return $progress;
+	}
+
 
     /**
-     * Removes stage
+     * Add skills
      *
-     * @param Stage $stage
-     */
-    public function removeStage(Stage $stage) {
-        $this->stages->removeElement($stage);
-    }
-
-    /**
-     * Get stage
-     *
-     * @return Collection 
-     */
-    public function getStages() {
-        return $this->stages;
-    }
-	
-
-    /**
-     * Add userProjects
-     *
-     * @param \AppBundle\Entity\UserProject $userProjects
+     * @param \AppBundle\Entity\Skill $skills
      * @return Project
      */
-    public function addUserProject(\AppBundle\Entity\UserProject $userProjects) {
-        $this->userProjects[] = $userProjects;
+    public function addSkill(\AppBundle\Entity\Skill $skills) {
+        $this->skills[] = $skills;
+		$skills->addProject($this);
         return $this;
     }
 
     /**
-     * Remove userProjects
+     * Remove skills
      *
-     * @param \AppBundle\Entity\UserProject $userProjects
+     * @param \AppBundle\Entity\Skill $skills
      */
-    public function removeUserProject(\AppBundle\Entity\UserProject $userProjects) {
-        $this->userProjects->removeElement($userProjects);
+    public function removeSkill(\AppBundle\Entity\Skill $skills) {
+        $this->skills->removeElement($skills);
     }
 
     /**
-     * Get userProjects
+     * Get skills
      *
      * @return \Doctrine\Common\Collections\Collection 
      */
-    public function getUserProjects() {
-        return $this->userProjects;
+    public function getSkills() {
+        return $this->skills;
     }
 }
