d599c6f643edc23ab85d31f36c8f4606ca1d8a32
Mickey <Mickey@10.0.170.120>
2015-03-13 15:35:15 +0100

    WTF

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -47,96 +47,97 @@ class AdminController extends Controller {
         return $this->render('AppBundle:Admin:import.html.twig');
     }
 
-    public function validateProjectAction($id) {
-        // If we have an id
-        if ($id > 0) {
-            $em = $this->getDoctrine()
-                    ->getManager();
-            // Select the project by his id
-            $project = $em->getRepository('AppBundle:Project')->find($id);
-            // Call the "toggleStatus" function who change the status of a project
-            $project->toggleStatus('valider');
-            $em->flush();
-            return $this->redirect(
-                            $this->generateUrl('admin_project')
-            );
-        } else {
-            throw $this->createNotFoundException('Projet N°' . $id . ' introuvable');
-        }
-    }
+	public function validateProjectAction($id) {
+		// If we have an id
+		if ($id > 0) {
+			$em = $this->getDoctrine()
+					->getManager();
+			// Select the project by his id
+			$project = $em->getRepository('AppBundle:Project')->find($id);
+			// Call the "toggleStatus" function who change the status of a project
+			$project->toggleStatus('valider');
+			$em->flush();
+			return $this->redirect(
+							$this->generateUrl('admin_project')
+			);
+		} else {
+			throw $this->createNotFoundException('Projet N°' . $id . ' introuvable');
+		}
+	}
 
-    public function addSkillAction(Request $req) {
-        $skill = new Skill;
+	public function addSkillAction(Request $req) {
+		$skill = new Skill;
 
-        $rep = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('AppBundle:Skill');
+		$rep = $this->getDoctrine()
+				->getManager()
+				->getRepository('AppBundle:Skill');
 
-        $em = $this->getDoctrine()
-                ->getManager();
-        // Determinate the id of the selected category
-        $id = $req->get('selectCat');
+		$em = $this->getDoctrine()
+				->getManager();
+		// Determinate the id of the selected category
+		$id = $req->get('selectCat');
 
-        // If the user has selected a category
-        if ($id > 0) {
-            // The added skill will have a parent which is the selected category
-            $skillParent = $rep->find($id);
-            // Setting the title and the parent to the added skill
-            $skill->setTitle($req->get('title'));
-            $skill->setParent($skillParent);
-            $em->persist($skill);
-            $em->flush();
-            return $this->redirect(
-                            $this->generateUrl('admin_skill'));
-        }
-        // If the user hasn't selected a category
-        else {
-            // the skill will be a category without parent
-            $skill->setTitle($req->get('title'));
-            $em->persist($skill);
-            $em->flush();
-            return $this->redirect(
-                            $this->generateUrl('admin_skill'));
-        }
-    }
+		// If the user has selected a category
+		if ($id > 0) {
+			// The added skill will have a parent which is the selected category
+			$skillParent = $rep->find($id);
+			// Setting the title and the parent to the added skill
+			$skill->setTitle($req->get('title'));
+			$skill->setParent($skillParent);
+			$em->persist($skill);
+			$em->flush();
+			return $this->redirect(
+							$this->generateUrl('admin_skill'));
+		}
+		// If the user hasn't selected a category
+		else {
+			// the skill will be a category without parent
+			$skill->setTitle($req->get('title'));
+			$em->persist($skill);
+			$em->flush();
+			return $this->redirect(
+							$this->generateUrl('admin_skill'));
+		}
+	}
 
-    public function deleteSkillAction($id) {
-        if ($id > 0) {
-            $em = $this->getDoctrine()
-                    ->getManager();
+	public function deleteSkillAction($id) {
+		if ($id > 0) {
+			$em = $this->getDoctrine()
+					->getManager();
 
-            $skill = $em->getRepository('AppBundle:Skill')->find($id);
-            $em->remove($skill);
-            $em->flush();
+			$skill = $em->getRepository('AppBundle:Skill')->find($id);
+			$em->remove($skill);
+			$em->flush();
 
-            return $this->redirect(
-                            $this->generateUrl('admin_skill')
-            );
-        } else {
-            throw $this->createNotFoundException('Skill N°' . $id . ' introuvable');
-        }
-    }
+			return $this->redirect(
+							$this->generateUrl('admin_skill')
+			);
+		} else {
+			throw $this->createNotFoundException('Skill N°' . $id . ' introuvable');
+		}
+	}
 
-    public function updateSkillAction(Request $req) {
-        $id = ($req->request->get('id'));
+	public function updateSkillAction(Request $req) {
+		$id = ($req->request->get('id'));
 
-        if ($id > 0) {
-            $em = $this->getDoctrine()
-                    ->getManager();
-            $skill = $em->getRepository('AppBundle:Skill')->find($id);
+		if ($id > 0) {
+			$em = $this->getDoctrine()
+					->getManager();
+			$skill = $em->getRepository('AppBundle:Skill')->find($id);
 
-            $skill->setTitle($req->get('title'));
+			$skill->setTitle($req->get('title'));
 
-            $em->merge($skill);
-            $em->flush();
+			$em->merge($skill);
+			$em->flush();
 
-            $response = new JsonResponse();
-            $response->setData(array(
-                'data' => 'ok'));
-            return $response;
-        } else {
-            throw $this->createNotFoundException('Skill N°' . $id . ' introuvable');
-        }
-    }
+			$response = new JsonResponse();
+			$response->setData(array(
+				'data' => 'ok'));
+			return $response;
+			
+		} else {
+			throw $this->createNotFoundException('Skill N°' . $id . ' introuvable');
+		}
+	}
 
 }
